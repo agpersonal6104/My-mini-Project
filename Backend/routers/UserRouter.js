0030b06b221918e1c59abe2b1698be1ee840e35e
AyushG <legendsteamprivate@gmail.com>
2024-09-07 22:10:57 +0530

    User Router completed

--- Backend/routers/UserRouter.js
+++ Backend/routers/UserRouter.js
@@ -13,3 +13,46 @@ router.post('/add', (req,res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/getall', (req,res) => {
+    Model.find()
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        res.status(500).json({
+            message : 'Something went Wrong!'
+        });
+    });
+});
+
+router.get('/getbyid/:id', (req,res) => {
+    Model.findById(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.put('/update/:id', (req,res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, {new : true})
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/delete/:id', (req,res) => {
+    Model.findByIdAndRemove(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
