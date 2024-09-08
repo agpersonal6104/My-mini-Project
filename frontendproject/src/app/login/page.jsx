92814c05971bca03b787a425ade405c2229b4e9a
AyushG <legendsteamprivate@gmail.com>
2024-09-08 18:51:39 +0530

    login form

--- frontendproject/src/app/login/page.jsx
+++ frontendproject/src/app/login/page.jsx
@@ -1,8 +1,13 @@
+'use client';
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center h-[90vh]'>
+        <div className='w-[40%] h-[60%] rounded-lg shadow-xl'>
+            <input type="text" className='' placeholder='Email'/>
+        </div>
+    </div>
   )
 }
 
