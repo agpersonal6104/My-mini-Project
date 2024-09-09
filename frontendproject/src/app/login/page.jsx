62759ad6a3fd6668f7a3654ede7f9a00cc3206ac
AyushG <legendsteamprivate@gmail.com>
2024-09-09 10:36:29 +0530

    login form

--- frontendproject/src/app/login/page.jsx
+++ frontendproject/src/app/login/page.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 const Login = () => {
   return (
     <div className='flex items-center justify-center h-[90vh]'>
-        <div className='w-[40%] h-[60%] rounded-lg shadow-xl'>
+        <div className='w-[25%] h-[60%] rounded-lg shadow-xl'>
             <div className='flex flex-col items-center justify-center h-[40%]'>
-                <h1 className='text-xl font-bold'>Log in with</h1>
+                <h1 className='p-6 m-2 text-xl font-bold'>Log in with</h1>
                 
-                <div className='grid grid-cols-2'>
-                    <button>Google</button>
-                    <button>Microsoft</button>
+                <div className='grid grid-cols-2 gap-6 mt-4'>
+                    <button className='px-8 py-4 bg-gray-200'>Google</button>
+                    <button className='px-8 py-4 bg-gray-200'>Microsoft</button>
                 </div>
                 
             </div>
