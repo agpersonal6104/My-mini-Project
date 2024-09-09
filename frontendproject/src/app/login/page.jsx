ffc1f6e8468beddfeb4968a365f1054f35a96d80
AyushG <legendsteamprivate@gmail.com>
2024-09-09 10:28:36 +0530

    login form

--- frontendproject/src/app/login/page.jsx
+++ frontendproject/src/app/login/page.jsx
@@ -5,6 +5,16 @@ const Login = () => {
   return (
     <div className='flex items-center justify-center h-[90vh]'>
         <div className='w-[40%] h-[60%] rounded-lg shadow-xl'>
+            <div className='flex flex-col items-center justify-center h-[40%]'>
+                <h1 className='text-xl font-bold'>Log in with</h1>
+                
+                <div className='grid grid-cols-2'>
+                    <button>Google</button>
+                    <button>Microsoft</button>
+                </div>
+                
+            </div>
+            
             <input type="text" className='' placeholder='Email'/>
         </div>
     </div>
