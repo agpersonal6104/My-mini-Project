f7b52f2502ab2566a3bff8ff1244cc406320fa4c
AyushG <legendsteamprivate@gmail.com>
2024-09-15 11:34:25 +0530

    Background image not working

--- frontendproject/src/app/page.jsx
+++ frontendproject/src/app/page.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='flex items-center justify-center mx-auto'>
-      <header className='flex'>
-        
+      <header className='grid h-[20%] grid-cols-2' id='backgroundHome'>
+        <div></div>
       </header>
     </div>
   )
