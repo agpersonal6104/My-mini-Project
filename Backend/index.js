2d159ee6ac1cb7a5fe258b33362f5e73db3b6bd4
AyushG <legendsteamprivate@gmail.com>
2024-09-16 12:14:50 +0530

    Blog Router connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 // Importing Express
 const express = require('express');
 const UserRouter = require('../Backend/routers/UserRouter');
+const BlogRouter = require('../Backend/routers/BlogRouter');
 const cors = require('cors');
 
 const app=express();
@@ -11,9 +12,13 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
-// 
+// User middleware
 app.use(express.json());
 app.use('/user',UserRouter);
+
+// Blog middleware
+app.use(express.json());
+app.use('/blog',BlogRouter);
 
 app.get('/', (req,res) => {
     res.send('Welcome to my project');
--- frontendproject/src/app/createBlog/page.jsx
+++ frontendproject/src/app/createBlog/page.jsx
@@ -1,20 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const CreateBlog = () => {
-  return (
-    <div className='h-[100vh] flex justify-center items-center'>
-        <div className=''>
-            <form>
-            <label htmlFor="title">Enter the Title</label>
-            <input type="text" />
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishedBy, setPublishedBy] = useState('');
+  const [image, setImage] = useState(null);
 
-            <label htmlFor="description">Enter the Description</label>
-            <input type="text" />
-            </form>
-        </div>
-        
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('description', description);
+    formData.append('author', author);
+    formData.append('publishedBy', publishedBy);
+    formData.append('image', image);
+
+    try {
+      const response = await axios.post('/blog', formData);
+      console.log(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleImageChange = (e) => {
+    setImage(e.target.files[0]);
+  };
+
+  return (
+    <div className="h-[100vh] flex justify-center items-center">
+      <div className="">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Enter the Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <label htmlFor="description">Enter the Description</label>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+
+          <label htmlFor="author">Enter the Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+
+          <label htmlFor="publishedBy">Enter the Published By</label>
+          <input
+            type="text"
+            value={publishedBy}
+            onChange={(e) => setPublishedBy(e.target.value)}
+          />
+
+          <label htmlFor="image">Select an Image</label>
+          <input type="file" onChange={handleImageChange} />
+
+          <button type="submit">Create Blog Post</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CreateBlog;
