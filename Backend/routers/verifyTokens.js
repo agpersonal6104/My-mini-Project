865cc1279f462aff28f0f47835ccd52d3240f78a
AyushG <legendsteamprivate@gmail.com>
2024-10-01 14:56:12 +0530

    Token verification active

--- Backend/routers/verifyTokens.js
+++ Backend/routers/verifyTokens.js
@@ -1,22 +1,17 @@
-const jwt = require('jasonwebtoken');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const verifyToken = (req,res,next) => {
-    const token = req.header['x-auth-token'];
-
-    if(!token)
-    {
-        return res.status(401).json({message: 'No Token Provided'});
+const verifyToken = (req, res, next) => {
+    const token = req.header['x-auth-token']; // added a closing bracket
+    if (!token) {
+        return res.status(401).json({ message: 'No Token Provided' });
     }
 
-    jwt.verify(token,process.env.JWT_SECRET, (err,payload) => {
-        if(err)
-        {
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+        if (err) {
             console.log(err);
-            return res.status(401).json({message: 'Token not Valid!'});
-        }
-        else
-        {
+            return res.status(401).json({ message: 'Token not Valid!' });
+        } else {
             req.user = payload;
             next();
         }
